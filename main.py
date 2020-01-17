0eb1f7797975e00659da487f1aff79d62470a1b3
OTHSCS107 <OTHSCS107@KATYISD.ORG>
2020-01-17 08:15:39 -0600

    changed some things

--- main.py
+++ main.py
@@ -4,15 +4,47 @@ import re
 import urllib
 
 
+def run():
+    r = requests.get("https://en.wikipedia.org/wiki/Quantum_mechanics")
+    r.raise_for_status()
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-r = requests.get("https://en.wikipedia.org/wiki/Quantum_mechanics")
-r.raise_for_status()
-soup = BeautifulSoup(r.text, 'html.parser')
+    a_tags= soup.findAll('a')
+    s= []
+    for tag in a_tags:
+        #print(len(tag.contents))
+        s.append(tag.get('href',None))
 
-a_tags= soup.findAll('a')
+    wikis= []
+    for i in range(len(s)):
+     if re.search(r'^/wiki/.+', str(s[i])):
+        wikis.append(s[i])
 
-for tag in a_tags:
-    print(tag.get('href',None))
+    s= "https://en.wikipedia.org"
+    for counter in range(len(wikis)):
+        s+=wikis[counter]
+        r1= requests.get(s)
+        souper(r1)
+        if counter==50:
+           break
+
+
+
+def souper(r1):
+    soup = BeautifulSoup(r1.text, 'html.parser')
+    a_tags= soup.findAll('a')
+
+    s= []
+    for tag in a_tags:
+        #print(len(tag.contents))
+        s.append(tag.get('href',None))
+        #goes to check if its a div class that has an image
+        soup.parent
+
+
+    return 0
+
+
 #print(soup)
 '''
 for tag in tags:
@@ -22,3 +54,4 @@ resource = urllib.request.urlopen("http://www.digimouth.com/news/media/2011/09/g
 output = open("file01.jpg","wb")
 output.write(resource.read())
 '''
+run()
