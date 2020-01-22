fa79e9e5bb9d92c710a399563b7c91f3539d4c65
OTHSCS107 <OTHSCS107@KATYISD.ORG>
2020-01-22 08:03:59 -0600

    web scraper grabs all non wikipedia links

--- main.py
+++ main.py
@@ -17,30 +17,45 @@ def run():
 
     wikis= []
     for i in range(len(s)):
-     if re.search(r'^/wiki/.+', str(s[i])):
-        wikis.append(s[i])
+     if re.search(r'^/wiki/.+', str(s[i])) or re.search(r'^https://en.wikipedia.org.+',str(s[i])):
+        continue
+     else:
+        if re.search(r'^https://.+',str(s[i])):
+         wikis.append(s[i])
+    
+    del wikis[2]
+    
+    print(wikis)
 
+    '''
+   
     s= "https://en.wikipedia.org"
     for counter in range(len(wikis)):
         s+=wikis[counter]
+        print(s)
         r1= requests.get(s)
-        souper(r1)
-        if counter==50:
+        #print(souper(r1))
+        if counter==16:
            break
-
+    '''
 
 
 def souper(r1):
     soup = BeautifulSoup(r1.text, 'html.parser')
-    a_tags= soup.findAll('a')
-
+    print(r1.text)
+    a_tags= soup.findAll('div',{'class':'thumbcaption'})
+    
+    print(len(a_tags))
     s= []
     for tag in a_tags:
         #print(len(tag.contents))
-        s.append(tag.get('href',None))
+        #s.append(tag.get('href',None))
         #goes to check if its a div class that has an image
-        soup.parent
-
+        if tag.parent== 'thumbinner':
+           print("s")
+        print(tag.child.text)
+       # strs= re.findall('^<a href=.*', tag.parent)
+       # print(len(strs))
 
     return 0
 
