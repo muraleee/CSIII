77e9d4142f0fbce180e45a1ce4a3b2206f9c8a76
OTHSCS107 <OTHSCS107@KATYISD.ORG>
2020-01-30 08:10:41 -0600

    changes made

--- webscraper_main.py
+++ webscraper_main.py
@@ -8,8 +8,9 @@ from scrapy.http import TextResponse
 def run():
     url = "https://sfbay.craigslist.org/search/sfc/apa"
 
-
-
+    housing_urls= []
+     
+    #goes through 24 pages of craigslist  
     for i in range(24):
         if i == 0:
             print(souper(url))
@@ -20,11 +21,23 @@ def souper(url):
     soup = BeautifulSoup(r.text, 'html.parser')
     ul_tags = soup.findAll('ul',{'class':'rows'})
     z=0
+
+    #supposed to have the urls for the 1 page
+    housing_urls= []
+    li_tags= soup.findAll('li',{'class':'result-row'})
+    for i in range(len(li_tags)):
+      for tag in li_tags[i].find_all('a'):
+        if i==0:
+           print(tag.get('href',None))
+    #need to find a way to isolate the link
+
+    #print(li_tags[0])
+    '''
     for child in ul_tags[0].children:
         if z==0:
           print(child)
-          li_tags= child.findAll('li')
-          
+          #s=re.findall('<a.+', child)
+    '''    
         
            
 
@@ -35,6 +48,6 @@ def souper(url):
               print(child)
             z+=1
     '''
-    return 0
+    return housing_urls
 
 run()
